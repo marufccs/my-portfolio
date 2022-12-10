8781fc4507ae7c9a455187d448e5ec1ca9f40dcc
unknown <marufmozumder7@gmail.com>
2022-12-10 15:37:21 +0100

    footer-added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
 import './App.css';
 import Banner from './components/Banner/Banner';
+import Footer from './components/Footer/Footer';
 import MyProjects from './components/MyProjects/MyProjects';
 import Resume from './components/Resume/Resume';
 
 function App() {
   return (
+    <div>
     <div className="mx-20">
      <Banner/>
      <Resume/>
      <MyProjects/>
+    </div>
+    <Footer/>
     </div>
   );
 }
