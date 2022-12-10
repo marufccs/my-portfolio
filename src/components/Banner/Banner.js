78b1cf2d746de70546b7c761da212361be5178c5
unknown <marufmozumder7@gmail.com>
2022-12-10 13:20:21 +0100

    banner-added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import maruf from '../../Pictures/Maruf.png'
 import './Banner.css'
 const Banner = () => {
     return (
-        <div className='flex justify-between'>
+        <div className='lg:flex justify-between'>
             <div>
             <section >
                 <span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>
@@ -11,16 +11,14 @@ const Banner = () => {
             </section>
             </div>
           <div>
-         
-            <div className='lg:text-6xl md:text-4xl font-extrabold lg:mt-36'>
+            <div className='sm:text-3xl lg:text-6xl  font-extrabold  lg:mt-36 starting-lines'>
 <h1 className='animate__animated animate__backInLeft'>Hello</h1>
-<h1 className='animate__animated animate__backInLeft animate__delay-1s'>I'm Maruf Mozumder</h1>
+<h1 className='animate__animated animate__backInLeft animate__delay-1s w-full'>I'm Maruf Mozumder</h1>
 <h1 className='animate__animated animate__backInLeft animate__delay-2s'>I'm a MERN Stack Developer</h1>
 </div>
           </div>
-          <div className=''>
-            <img src={maruf} width='1000' height='800' alt="Maruf-Mozumder" />
-           
+          <div>
+            <img className='image' src={maruf} width='1000' height='800' alt="Maruf-Mozumder" />
           </div>
 
         </div>
